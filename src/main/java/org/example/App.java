5cf2f87f28987f1ad0806aba95f59cb780a0ba08
Jamdu <Martinez.josh506@gmail.com>
2021-09-12 17:14:26 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,27 @@
+package org.example;
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner scnr = new Scanner(System.in);
+
+        System.out.println("What is the first number? ");
+        int x = scnr.nextInt();
+
+        System.out.println("What is the second number? ");
+        int y = scnr.nextInt();
+
+        int add = x + y;
+        int sub = x - y;
+        int multiply = x * y;
+        int div = x / y;
+
+        System.out.format("%d + %d = %d\n", x, y, add);
+        System.out.format("%d - %d = %d\n", x, y, sub);
+        System.out.format("%d * %d = %d\n", x, y, multiply);
+        System.out.format("%d / %d = %d", x, y, div);
+
+    }
+}
